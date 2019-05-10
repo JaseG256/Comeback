7ad6d2b3ac344f6a33fb3fd528f7d554e4c5437e
JaseG256 <jason_r_gibbs@yahoo.com>
2019-05-10 09:38:52 -0700

    initial commit

---
+++ converse/converse/src/main/java/com/msa/converse/model/Comment.java
@@ -0,0 +1,50 @@
+package com.msa.converse.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String text;
+
+    private Date timeSent;
+
+    public Comment() { }
+
+    public Comment(String text) {
+        this.text = text;
+        this.timeSent = new Date();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Date getTimeSent() {
+        return timeSent;
+    }
+
+    public void setTimeSent(Date timeSent) {
+        this.timeSent = timeSent;
+    }
+}
